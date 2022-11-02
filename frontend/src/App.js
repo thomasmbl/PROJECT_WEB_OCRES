b70a40ae58f8bf1444d2cd6a598cf18c9126469e
thomasmbl <91416520+thomasmbl@users.noreply.github.com>
2022-11-02 12:44:10 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [charac, setCharac] = useState([]);
+
+  const fetchData = () => {
+    return fetch("https://www.breakingbadapi.com/api/characters?name=Walter+White")
+          .then((response) => response.json())
+          .then((data) => setCharac(data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[])
+
   return (
-    <div className="App">
-          A vous de jouer !
-    </div>
+    <body>
+      <header>
+        <img className="img" src={require('./Breaking-Bad.jpg')} alt="banner"/>
+        
+      </header>
+
+      <div className="Character">
+        {charac && charac.length > 0 && charac.map((characObj, index) => (
+            <><h1 key={characObj.char_id}>{characObj.name}</h1>
+            <img key={characObj.char_id} src={characObj.img} alt="img"></img></>
+        ))}
+      </div>
+      
+
+
+    </body>
+    
   );
 }
 
