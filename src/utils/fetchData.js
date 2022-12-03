92c894436fb55fc53c80a83498043a0f57183ac6
thomasmbl <91416520+thomasmbl@users.noreply.github.com>
2022-12-03 19:51:26 +0100

    Delete src directory

--- src/utils/fetchData.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const fetchData = async (text) => {
-    return await axios.get(`https://breakingbadapi.com/api/characters?name=${text}`)
-}
-
-export default fetchData;
